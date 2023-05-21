41040d1776e6c1f079be1a041bbb53ca531407d7
Youngseo Choi <ys550499@gmail.com>
2023-05-21 15:32:24 +0900

    Simple CRUD

---
+++ src/main/java/more/practice/armeriaspring/controller/BlogController.java
@@ -0,0 +1,55 @@
+package more.practice.armeriaspring.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RestController
+@RequestMapping("/blogs")
+public class BlogController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BlogController.class);
+    private final Map<Long, BlogPost> blogPosts = new ConcurrentHashMap<>();
+
+    @GetMapping("/{id}")
+    public BlogPost get(@PathVariable Long id) {
+        logger.debug(blogPosts.get(id).toString());
+        return blogPosts.get(id);
+    }
+
+    @GetMapping
+    public List<BlogPost> getAll() {
+        return blogPosts.values().stream().toList();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BlogPost create(@RequestBody BlogPost post) {
+        logger.debug(post.toString());
+        blogPosts.put(post.getId(), post);
+        return post;
+    }
+
+    @PutMapping("/{id}")
+    public BlogPost update(@PathVariable Long id, @RequestBody BlogPost post) {
+        if (id != post.getId()) {
+            throw new IllegalArgumentException("The id of post is not same with given id in the path parameter.");
+        }
+        blogPosts.put(id, post);
+        return post;
+    }
+
+    @DeleteMapping("/{id}")
+    public BlogPost delete(@PathVariable Long id) {
+        if (!blogPosts.containsKey(id)) {
+            throw new IllegalArgumentException("Id is not exist.");
+        }
+        return blogPosts.remove(id);
+    }
+
+}
--- src/main/java/more/practice/armeriaspring/controller/BlogPost.java
+++ src/main/java/more/practice/armeriaspring/controller/BlogPost.java
@@ -1,11 +1,23 @@
 package more.practice.armeriaspring.controller;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class BlogPost {
     private final long id;
     private final String title;
     private final String content;
     private final long createdAt;
     private final long updatedAt;
+
+    @JsonCreator
+    public BlogPost(@JsonProperty("id") long id, @JsonProperty("title") String title, @JsonProperty("content") String content) {
+        this(id, title, content, System.currentTimeMillis());
+    }
+
+    public BlogPost(long id, String title, String content, long createdAt) {
+        this(id, title, content, createdAt, createdAt);
+    }
 
     public BlogPost(long id, String title, String content, long createdAt, long updatedAt) {
         this.id = id;
@@ -33,5 +45,16 @@ public class BlogPost {
 
     public long getUpdatedAt() {
         return updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "BlogPost{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
     }
 }
