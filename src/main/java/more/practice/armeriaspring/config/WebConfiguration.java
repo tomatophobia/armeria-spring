af16a394dc79863d2c352332eb2d7641d9cabefa
Youngseo Choi <ys550499@gmail.com>
2023-05-22 22:04:33 +0900

    기존 Spring Tomcat Service를 아르메리아 레이어 위로 옮기기

---
+++ src/main/java/more/practice/armeriaspring/config/WebConfiguration.java
@@ -0,0 +1,36 @@
+package more.practice.armeriaspring.config;
+
+import com.linecorp.armeria.server.healthcheck.HealthChecker;
+import com.linecorp.armeria.server.tomcat.TomcatService;
+import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+    public static Connector getConnector(ServletWebServerApplicationContext applicationContext) {
+        final TomcatWebServer container = (TomcatWebServer) applicationContext.getWebServer();
+
+        // Start the container to make sure all connectors are available.
+        container.start();
+        return container.getTomcat().getConnector();
+    }
+
+    @Bean
+    public HealthChecker tomcatConnectorHealthChecker(ServletWebServerApplicationContext applicationContext) {
+        final Connector connector = getConnector(applicationContext);
+        return () -> connector.getState().isAvailable();
+    }
+
+    @Bean
+    public TomcatService tomcatService(ServletWebServerApplicationContext applicationContext) {
+        return TomcatService.of(getConnector(applicationContext));
+    }
+    @Bean
+    public ArmeriaServerConfigurator armeriaServerConfigurator(TomcatService tomcatService) {
+        return sb -> sb.service("prefix:/", tomcatService);
+    }
+}
